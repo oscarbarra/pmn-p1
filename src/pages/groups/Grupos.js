45f03e284540b1d808f805bd8fbdfe05c342326a
obarra <obarra2023@alu.uct.cl>
2025-04-27 06:07:37 -0400

    modulo de grupos maquetado

--- src/pages/groups/Grupos.js
+++ src/pages/groups/Grupos.js
@@ -1,8 +1,47 @@
 import './Grupos.css';
+import ViewGroups from '../../components/viewGroups/ViewGroups';
 
 function Grupos() {
   return (
-    <h1> Grupos de trabajo</h1>
+    <div className="grupos-container">
+      <div className="header-row">
+        <div className="header-content">
+          <h3 className="header-title">Inicio</h3>
+          <p className="header-description">
+            Revisa los grupos a los que perteneces
+          </p>
+        </div>
+      </div>
+
+      <div className="groups-row">
+        {/* Grupo 1 */}
+        <ViewGroups nombre={"Grupo 1"} area={"administración"} />
+
+        {/* Grupo 2 */}
+        <ViewGroups nombre={"Grupo 2"} area={"administración"} />
+
+        {/* Grupo 3 */}
+        <ViewGroups nombre={"Grupo 3"} area={"administración"} />
+
+        {/* Grupo 4 */}
+        <ViewGroups nombre={"Grupo 4"} area={"administración"} />
+
+        {/* Grupo 5 */}
+        <ViewGroups nombre={"Grupo 5"} area={"administración"} />
+
+        {/* Grupos 6 */}
+        <ViewGroups nombre={"Grupo 6"} area={"administración"} />
+
+        {/* Grupo 7 */}
+        <ViewGroups nombre={"Grupo 7"} area={"administración"} />
+
+        {/* Grupo 8 */}
+        <ViewGroups nombre={"Grupo 8"} area={"administración"} />
+
+        {/* Grupo 9 */}
+        <ViewGroups nombre={"Grupo 9"} area={"administración"} />
+      </div>
+    </div>
   );
 }
 
